81c0459b458838d24990209d612cf343e0250d97
Jay Williams <jay@myd3.com>
2014-08-11 11:02:24 -0500

    Use braces for all IF statements, plus extra white space

--- indieweb/PuSH/Publisher.php
+++ indieweb/PuSH/Publisher.php
@@ -17,11 +17,13 @@ class Publisher
     // create a new Publisher
     public function __construct($hub_url)
     {
-        if (!isset($hub_url))
+        if (!isset($hub_url)) {
             throw new Exception('Please specify a hub url');
+        }
 
-        if (!preg_match("|^https?://|i",$hub_url))
+        if (!preg_match("|^https?://|i", $hub_url)) {
             throw new Exception('The specified hub url does not appear to be valid: '.$hub_url);
+        }
 
         $this->hub_url = $hub_url;
     }
@@ -29,8 +31,9 @@ class Publisher
     // accepts either a single url or an array of urls
     public function publish_update($topic_urls, $http_function = false)
     {
-        if (!isset($topic_urls))
+        if (!isset($topic_urls)) {
             throw new Exception('Please specify a topic url');
+        }
 
         // check that we're working with an array
         if (!is_array($topic_urls)) {
@@ -39,12 +42,14 @@ class Publisher
 
         // set the mode to publish
         $post_string = "hub.mode=publish";
+
         // loop through each topic url
         foreach ($topic_urls as $topic_url) {
 
             // lightweight check that we're actually working w/ a valid url
-            if (!preg_match("|^https?://|i",$topic_url))
+            if (!preg_match("|^https?://|i", $topic_url)) {
                 throw new Exception('The specified topic url does not appear to be valid: '.$topic_url);
+            }
 
             // append the topic url parameters
             $post_string .= "&hub.url=".urlencode($topic_url);
@@ -52,10 +57,11 @@ class Publisher
 
         // make the http post request and return true/false
         // easy to over-write to use your own http function
-        if ($http_function)
-            return $http_function($this->hub_url,$post_string);
-        else
-            return $this->http_post($this->hub_url,$post_string);
+        if ($http_function) {
+            return $http_function($this->hub_url, $post_string);
+        } else {
+            return $this->http_post($this->hub_url, $post_string);
+        }
     }
 
     // returns any error message from the latest request
@@ -68,10 +74,10 @@ class Publisher
     private function http_post($url, $post_string)
     {
         // add any additional curl options here
-        $options = array(CURLOPT_URL => $url,
-                         CURLOPT_POST => true,
+        $options = array(CURLOPT_URL        => $url,
+                         CURLOPT_POST       => true,
                          CURLOPT_POSTFIELDS => $post_string,
-                         CURLOPT_USERAGENT => "PubSubHubbub-Publisher-PHP/1.0");
+                         CURLOPT_USERAGENT  => "PubSubHubbub-Publisher-PHP/1.0");
 
         $ch = curl_init();
         curl_setopt_array($ch, $options);
@@ -83,8 +89,10 @@ class Publisher
         curl_close($ch);
 
         // all good
-        if ($info['http_code'] == 204)
+        if ($info['http_code'] == 204) {
             return true;
+        }
+
         return false;
     }
 }
